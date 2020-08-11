742e459f6935ce23fe0a1734d6845fcdb60f553a
Onberfrest <62020954+Onberfrest@users.noreply.github.com>
2020-08-11 12:32:16 +0300

    style

--- dice/src/Die.js
+++ dice/src/Die.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import './Die.css';
 
 export default class Die extends Component {
 	render() {
-		return <i className={`fas fa-dice-${this.props.side}`} />;
+		return <i className={`Die fas fa-dice-${this.props.side}`} />;
 	}
 }
