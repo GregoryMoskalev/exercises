201a762eb1b687d05cec8f621cb8d90fa833d4fd
Onberfrest <62020954+Onberfrest@users.noreply.github.com>
2020-08-18 15:56:54 +0300

    createBoard function

--- react-lights-out/src/Board.js
+++ react-lights-out/src/Board.js
@@ -48,31 +48,38 @@ class Board extends Component {
 	createBoard() {
 		let board = [];
 		// TODO: create array-of-arrays of true/false values
+		for (let y = 0; y < this.props.nrows; y++) {
+			let row = [];
+			for (let x = 0; x < this.props.ncols; x++) {
+				row.push(Math.random() > this.props.chanceLightStartsOn);
+			}
+			board.push(row);
+		}
 		return board;
 	}
 
 	/** handle changing a cell: update board & determine if winner */
 
-	flipCellsAround(coord) {
-		let { ncols, nrows } = this.props;
-		let board = this.state.board;
-		let [ y, x ] = coord.split('-').map(Number);
+	// flipCellsAround(coord) {
+	// 	let { ncols, nrows } = this.props;
+	// 	let board = this.state.board;
+	// 	let [ y, x ] = coord.split('-').map(Number);
 
-		function flipCell(y, x) {
-			// if this coord is actually on board, flip it
+	// 	function flipCell(y, x) {
+	// 		// if this coord is actually on board, flip it
 
-			if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
-				board[y][x] = !board[y][x];
-			}
-		}
+	// 		if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
+	// 			board[y][x] = !board[y][x];
+	// 		}
+	// 	}
 
-		// TODO: flip this cell and the cells around it
+	// 	// TODO: flip this cell and the cells around it
 
-		// win when every cell is turned off
-		// TODO: determine is the game has been won
+	// 	// win when every cell is turned off
+	// 	// TODO: determine is the game has been won
 
-		this.setState({ board, hasWon });
-	}
+	// 	this.setState({ board, hasWon });
+	// }
 
 	/** Render game board or winning message. */
 
@@ -81,6 +88,11 @@ class Board extends Component {
 		// TODO
 		// make table board
 		// TODO
+		return (
+			<table>
+				<tr />
+			</table>
+		);
 	}
 }
 
