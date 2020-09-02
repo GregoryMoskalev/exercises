96393d684236f217906c0e5c5b9be720c713e9e6
Onberfrest <62020954+Onberfrest@users.noreply.github.com>
2020-09-02 17:56:55 +0300

    fixing dice locking bug

--- yahtzee/src/Die.js
+++ yahtzee/src/Die.js
@@ -1,18 +1,27 @@
-import React, { Component } from "react";
-import "./Die.css";
+import React, { Component } from 'react';
+import './Die.css';
 
 class Die extends Component {
-  render() {
-    return (
-      <button
-        className={"Die"}
-        style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
-        onClick={this.props.handleClick}
-      >
-        {this.props.val}
-      </button>
-    );
-  }
+	constructor(props) {
+		super(props);
+
+		this.onClick = this.onClick.bind(this);
+	}
+
+	onClick() {
+		this.props.handleClick(this.props.idx);
+	}
+	render() {
+		return (
+			<button
+				className={'Die'}
+				style={{ backgroundColor: this.props.locked ? 'grey' : 'black' }}
+				onClick={this.onClick}
+			>
+				{this.props.val}
+			</button>
+		);
+	}
 }
 
 export default Die;
