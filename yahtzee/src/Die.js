2ec3972d045d158f510937383dc675c614f4c386
Onberfrest <62020954+Onberfrest@users.noreply.github.com>
2021-02-11 22:01:42 +0300

    feat: dice icons

--- yahtzee/src/Die.js
+++ yahtzee/src/Die.js
@@ -1,27 +1,44 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faDiceOne,
+  faDiceTwo,
+  faDiceThree,
+  faDiceFour,
+  faDiceFive,
+  faDiceSix,
+} from '@fortawesome/free-solid-svg-icons';
 import './Die.css';
 
 class Die extends Component {
-	constructor(props) {
-		super(props);
+  static defaultProps = {
+    numberWords: [faDiceOne, faDiceTwo, faDiceThree, faDiceFour, faDiceFive, faDiceSix],
+    val: 5,
+  };
+  constructor(props) {
+    super(props);
 
-		this.onClick = this.onClick.bind(this);
-	}
+    this.onClick = this.onClick.bind(this);
+  }
 
-	onClick() {
-		this.props.handleClick(this.props.idx);
-	}
-	render() {
-		return (
-			<button
-				className={'Die'}
-				style={{ backgroundColor: this.props.locked ? 'grey' : 'black' }}
-				onClick={this.onClick}
-			>
-				{this.props.val}
-			</button>
-		);
-	}
+  onClick() {
+    this.props.handleClick(this.props.idx);
+  }
+  render() {
+    const { numberWords, val, disabled, locked, rolling } = this.props;
+    let classes = 'Die ';
+    if (locked) classes += 'Die-locked';
+    if (rolling) classes += 'Die-rolling';
+    return (
+      <FontAwesomeIcon
+        icon={numberWords[val - 1]}
+        className={classes}
+        size={'5x'}
+        disabled={disabled}
+        onClick={this.onClick}
+      />
+    );
+  }
 }
 
 export default Die;
