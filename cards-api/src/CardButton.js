222142c468de795eece9c43ba7b751c96bfac8c2
Onberfrest <62020954+Onberfrest@users.noreply.github.com>
2021-02-13 23:43:17 +0300

    feat: fetching card with axios

--- cards-api/src/CardButton.js
+++ cards-api/src/CardButton.js
@@ -1,2 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+
+class CardButton extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.props.getCard();
+  }
+
+  render() {
+    return <button onClick={this.handleClick}>Get Card!</button>;
+  }
+}
+export default CardButton;
--- cards-api/src/Deck.js
+++ cards-api/src/Deck.js
@@ -1,20 +1,44 @@
 import React, { Component } from 'react';
+import CardButton from './CardButton';
 import axios from 'axios';
-const API_URL = 'https://deckofcardsapi.com/api/deck/new/shuffle';
+const API_BASE_URL = 'https://deckofcardsapi.com/api/deck';
 
 class Deck extends Component {
   constructor(props) {
     super(props);
-    this.state = { deck: null };
+    this.state = { deck: null, drawn: [] };
+    this.getCard = this.getCard.bind(this);
   }
   async componentDidMount() {
-    const deck = await axios.get(API_URL);
+    const deck = await axios.get(`${API_BASE_URL}/new/shuffle`);
     this.setState({ deck: deck.data });
+  }
+
+  async getCard() {
+    let id = this.state.deck.deck_id;
+    try {
+      let cardUrl = `${API_BASE_URL}/${id}/draw/`;
+      let cardRes = await axios.get(cardUrl);
+      if (!cardRes.data.success) {
+        throw new Error('No cards remaining!');
+      }
+      console.log(cardRes.data);
+      let card = cardRes.data.cards[0];
+      this.setState((st) => ({
+        drawn: [
+          ...st.drawn,
+          { id: card.code, image: card.image, name: `${card.suit} ${card.value}` },
+        ],
+      }));
+    } catch (err) {
+      alert(err);
+    }
   }
   render() {
     return (
       <div>
         <h1>Card Dealer</h1>
+        <CardButton getCard={this.getCard} />
       </div>
     );
   }
