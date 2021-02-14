85624a540920a108b3958b039af5ff553d9a8352
Onberfrest <62020954+Onberfrest@users.noreply.github.com>
2021-02-14 19:17:06 +0300

    feat: adding dynamic colors and emojis

--- dad-jokes-project/src/Joke.js
+++ dad-jokes-project/src/Joke.js
@@ -4,21 +4,53 @@ import { faArrowUp, faArrowDown } from '@fortawesome/free-solid-svg-icons';
 import './Joke.css';
 
 class Joke extends Component {
+  getColor() {
+    if (this.props.votes >= 15) {
+      return '#4caf50';
+    } else if (this.props.votes >= 12) {
+      return '#8bc34a';
+    } else if (this.props.votes >= 9) {
+      return '#cddc39';
+    } else if (this.props.votes >= 6) {
+      return '#ffeb3b';
+    } else if (this.props.votes >= 3) {
+      return '#ffc107';
+    } else if (this.props.votes >= 0) {
+      return '#ff9800';
+    } else {
+      return '#f44336';
+    }
+  }
+  getEmoji() {
+    if (this.props.votes >= 15) {
+      return 'em em-rolling_on_the_floor_laughing';
+    } else if (this.props.votes >= 12) {
+      return 'em em-laughing';
+    } else if (this.props.votes >= 9) {
+      return 'em em-smiley';
+    } else if (this.props.votes >= 6) {
+      return 'em em-slightly_smiling_face';
+    } else if (this.props.votes >= 3) {
+      return 'em em-neutral_face';
+    } else if (this.props.votes >= 0) {
+      return 'em em-confused';
+    } else {
+      return 'em em-angry';
+    }
+  }
   render() {
     return (
       <div className="Joke">
         <div className="Joke-buttons">
           <FontAwesomeIcon className="icon-up" icon={faArrowUp} onClick={this.props.upvote} />
-          <span className="Joke-votes">{this.props.votes}</span>
+          <span className="Joke-votes" style={{ borderColor: this.getColor() }}>
+            {this.props.votes}
+          </span>
           <FontAwesomeIcon className="icon-down" icon={faArrowDown} onClick={this.props.downvote} />
         </div>
         <div className="Joke-text">{this.props.text}</div>
         <div className="Joke-smiley">
-          <i
-            class="em em-rolling_on_the_floor_laughing"
-            aria-role="presentation"
-            aria-label="ROLLING ON THE FLOOR LAUGHING"
-          />
+          <i className={this.getEmoji()} />
         </div>
       </div>
     );
